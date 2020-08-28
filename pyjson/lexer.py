66bfdce60175546e4bfac4546912e9569dc14180
yhiraki <coffexpr@gmail.com>
2020-08-28 15:01:16 +0900

    lexer

---
+++ pyjson/lexer.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+
+JSON_QUOTE = '"'
+
+JSON_OPENBRACE = '{'
+JSON_CLOSEBRACE = '}'
+JSON_OPENBRACKET = '['
+JSON_CLOSEBRACKET = ']'
+JSON_COLON = ':'
+JSON_COMMA = ','
+JSON_WHITESPACE = ' \t\b\n\r'
+
+JSON_TRUE = 'true'
+JSON_FALSE = 'false'
+JSON_NULL = 'null'
+
+JSON_SYNTAX = (
+    JSON_OPENBRACE,
+    JSON_CLOSEBRACE,
+    JSON_OPENBRACKET,
+    JSON_CLOSEBRACKET,
+    JSON_COLON,
+    JSON_COMMA,
+)
+
+JSON_ESCAPED = {
+    '"': '"',
+    '\\': '\\',
+    '/': '/',
+    'b': '\b',
+    'f': '\f',
+    'n': '\n',
+    'r': '\r',
+    't': '\t',
+    'u': '\\u'
+}
+
+
+def lex_string(i, string):
+    if string[i] != JSON_QUOTE:
+        return i, None
+
+    ret = ''
+    j = i + 1
+    while j < len(string) and string[j] != JSON_QUOTE:
+        if string[j] == '\\':
+            j += 1
+            if j >= len(string):
+                raise
+            c = JSON_ESCAPED.get(string[j])
+            if not c:
+                raise Exception(f'Unexpected escape charactor \\{string[j]}')
+            ret += c
+            j += 1
+            continue
+        ret += string[j]
+        j += 1
+
+    if j >= len(string) or string[j] != JSON_QUOTE:
+        raise Exception('Expected end-of-string quote')
+
+    return j + 1, ret
+
+
+def lex_number(i, string):
+    numbers = [str(i) for i in range(10)]
+    symbols = ['.']
+    number_chars = numbers + symbols
+
+    is_float = False
+
+    j = i
+    while j < len(string) and string[j] in number_chars:
+        if string[j] == '.':
+            if is_float:
+                raise Exception('invalid float')
+            is_float = True
+        j += 1
+
+    if j == i:
+        return j, None
+
+    json_number = string[i:j]
+
+    if is_float:
+        if len(json_number) == 1:
+            raise Exception('invalid float')
+        return j, float(json_number)
+
+    return j, int(json_number)
+
+
+def lex_bool(i, string):
+    if string[i] not in (JSON_TRUE[0], JSON_FALSE[0]):
+        return i, None
+
+    for b, ret in ((JSON_TRUE, True), (JSON_FALSE, False)):
+        len_b = len(b)
+        if string[i:i + len_b] == b:
+            return i + len_b, ret
+
+    return i, None
+
+
+def lex_null(i, string):
+    if string[i] not in JSON_NULL[0]:
+        return i, None
+
+    for b, ret in ((JSON_NULL, None), ):
+        len_b = len(b)
+        if string[i:i + len_b] == b:
+            return i + len_b, ret
+
+
+def skip_whitespace(i, string):
+    while i < len(string) and string[i] in JSON_WHITESPACE:
+        i += 1
+    return i
+
+
+def lex(string):
+    tokens = []
+
+    i = 0
+    while i < len(string):
+        lexers = (
+            lex_string,
+            lex_number,
+            lex_bool,
+            lex_null,
+        )
+        lexed = False
+        for lexer in lexers:
+            j, value = lexer(i, string)
+            if i < j:
+                tokens.append(value)
+                i = j
+                lexed = True
+                break
+        if lexed:
+            continue
+
+        j = skip_whitespace(i, string)
+        if i < j:
+            i = j
+            continue
+
+        if string[i] in JSON_SYNTAX:
+            tokens.append(string[i])
+            i += 1
+            continue
+
+        raise Exception(f'Unexpected charactor: {string[i]}')
+    return tokens
---
+++ tests/test_lexer.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from pyjson import lexer
+
+
+class TestLexString(unittest.TestCase):
+    def test_lex_string(self):
+        tests = [
+            ('"a"', (3, 'a')),
+            ('"a"', (3, 'a')),
+            ('true', (0, None)),
+        ]
+        for test, expect in tests:
+            self.assertEqual(lexer.lex_string(0, test), expect)
+
+    def test_lex_escaped(self):
+        tests = [
+            (r'"\"a"', (5, '"a')),
+            (r'"\uxxx"', (7, r'\uxxx')),
+            (r'"\\"', (4, '\\')),
+            (r'"\t"', (4, '\t')),
+        ]
+        for test, expect in tests:
+            self.assertEqual(lexer.lex_string(0, test), expect)
+
+    def test_lex_fail_end_of_string_quote(self):
+        tests = [
+            '"hoge',
+            '"',
+        ]
+        for test in tests:
+            with self.assertRaisesRegex(Exception, 'quote'):
+                lexer.lex_string(0, test)
+
+    def test_lex_fail_unescaped_charactor(self):
+        tests = [
+            r'"\h"',
+            r'"\a"',
+        ]
+        for test in tests:
+            with self.assertRaisesRegex(Exception, 'escape'):
+                r = lexer.lex_string(0, test)
+                print(r)
+
+
+class TestLexNumber(unittest.TestCase):
+    def test_lex_number(self):
+        tests = [
+            ('1', (1, 1)),
+            ('10', (2, 10)),
+            ('1.0', (3, 1.0)),
+            ('0.1', (3, 0.1)),
+            ('.1', (2, 0.1)),
+            ('a', (0, None)),
+        ]
+        for test, expected in tests:
+            self.assertEqual(lexer.lex_number(0, test), expected)
+
+    def test_lex_fail_invalid_float(self):
+        tests = [
+            '1.1.1',
+            '1..1',
+            '.',
+            '..',
+        ]
+        for test in tests:
+            with self.assertRaisesRegex(Exception, 'invalid float'):
+                lexer.lex_number(0, test)
+
+
+class TestLexBool(unittest.TestCase):
+    def test_lex_bool(self):
+        tests = [
+            ('true', (4, True)),
+            ('false', (5, False)),
+            ('e', (0, None)),
+        ]
+        for test, expected in tests:
+            self.assertEqual(lexer.lex_bool(0, test), expected)
+
+
+class TestLexNull(unittest.TestCase):
+    def test_lex_null(self):
+        tests = [
+            ('null', (4, None)),
+            ('a', (0, None)),
+        ]
+        for test, expected in tests:
+            self.assertEqual(lexer.lex_null(0, test), expected)
+
+
+class TestLexer(unittest.TestCase):
+    def test_lex_success(self):
+        tests = [
+            ('{}', ['{', '}']),
+            ('[]', ['[', ']']),
+            ('{ }', ['{', '}']),
+            ('{"hoge"}', ['{', 'hoge', '}']),
+            (r'{"\"hoge"}', ['{', '"hoge', '}']),
+            ('[{"hoge"}]', ['[', '{', 'hoge', '}', ']']),
+            ('{ "hoge" }', ['{', 'hoge', '}']),
+            (' {"hoge"} ', ['{', 'hoge', '}']),
+            (' {""} ', ['{', '', '}']),
+            ('{"hoge": 1}', ['{', 'hoge', ':', 1, '}']),
+            ('{"hoge": 1.0}', ['{', 'hoge', ':', 1.0, '}']),
+            ('{"hoge": .1}', ['{', 'hoge', ':', 0.1, '}']),
+            ('{"hoge": true}', ['{', 'hoge', ':', True, '}']),
+            ('{"hoge": null}', ['{', 'hoge', ':', None, '}']),
+            ('{"hoge": "fuga", "piyo": 1}',
+             ['{', 'hoge', ':', 'fuga', ',', 'piyo', ':', 1, '}']),
+        ]
+        for test, expected in tests:
+            self.assertEqual(lexer.lex(test), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
